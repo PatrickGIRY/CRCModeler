14ebfeac8c5f1a77c23674a120bb7297e519fc7e
Patrick GIRY <patrick.giry@vidal.fr>
2017-03-30 14:50:50 +0200

    refactor(CRCModeler): add command matcher

--- src/main/java/crc/modeler/Main.java
+++ src/main/java/crc/modeler/Main.java
@@ -11,7 +11,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UncheckedIOException;
 import java.nio.file.Paths;
-import java.util.Objects;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -20,20 +23,19 @@ public class Main {
 
         CommandHandler commandHandler = new CommandHandler();
         EventStore eventStore = new FileEventStore(Paths.get("data.eventstore"));
+        List<CommandMatcher> commandMatchers = Arrays.asList(
+                new CommandMatcher(Pattern.compile("^new$"), line -> commandHandler.handleCommand(
+                        null, new CreateCRCCard(CRCCardId.nextCRCCardId()),
+                        eventStore,
+                        (state, event) -> state, (state1, state2) -> state1)),
+                new CommandMatcher(Pattern.compile("^quit$"), line -> System.exit(0))
+        );
 
         try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
             do {
                 System.out.print("$ ");
                 final String line = bufferedReader.readLine();
-                if (Objects.equals("new", line)) {
-                    commandHandler.handleCommand(null, new CreateCRCCard(CRCCardId.nextCRCCardId()),
-                            eventStore,
-                            (state, event) -> state, (state1, state2) -> state1);
-                }
-                if (Objects.equals("quit", line)) {
-                    System.exit(0);
-                }
-
+                commandMatchers.stream().filter(cm -> cm.isSelected(line)).forEach(cm -> cm.execute(line));
             } while (true);
         } catch (IOException ioe) {
             throw new UncheckedIOException(ioe);
@@ -41,5 +43,26 @@ public class Main {
 
     }
 
+    private static class CommandMatcher {
+        private final Predicate<String> selector;
+        private final Executable action;
 
+        private CommandMatcher(Pattern selector, Executable action) {
+            this.selector = selector.asPredicate();
+            this.action = action;
+        }
+
+        private boolean isSelected(String line) {
+            return selector.test(line);
+        }
+
+        private void execute(String line) {
+            action.execute(line);
+        }
+
+        @FunctionalInterface
+        private interface Executable {
+            void execute(String line);
+        }
+    }
 }
